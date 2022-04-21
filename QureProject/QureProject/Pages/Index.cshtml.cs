4515c7b39a95167c4a7f6ed01d459f182f45f37e
VladyslavStashkevych <vladyslav.stashkevych.it.2021@lpnu.ua>
2022-04-21 23:30:57 +0300

    COMMIT

---
+++ QureProject/QureProject/Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QureProject.Pages {
+	public class IndexModel : PageModel {
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger) {
+			_logger = logger;
+		}
+
+		public void OnGet() {
+
+		}
+	}
+}
