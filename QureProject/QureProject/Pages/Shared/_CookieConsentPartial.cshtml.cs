c7d3c7db22e02fc0d6731c2fd7dd220ae8a3d445
VladyslavStashkevych <vladyslav.stashkevych.it.2021@lpnu.ua>
2022-04-29 15:48:11 +0300

    cookies

---
+++ QureProject/QureProject/Pages/Shared/_CookieConsentPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QureProject.Pages.Shared
+{
+    public class _CookieConsentPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
