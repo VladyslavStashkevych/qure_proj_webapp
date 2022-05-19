666ed98982fb07fd0ae0a9d72b60452964cde1ba
VladyslavStashkevych <vladyslav.stashkevych.it.2021@lpnu.ua>
2022-05-19 17:34:42 +0300

    mvc implemented

---
+++ QureProject/QureProject/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using QureProjectMVC.Models;
+
+namespace QureProjectMVC.Controllers {
+	public class HomeController : Controller {
+		private readonly ILogger<HomeController> _logger;
+		private readonly IWebHostEnvironment _environment;
+
+		public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment) {
+			_logger = logger;
+			_environment = environment;
+		}
+
+		public IActionResult Index() {
+			const string cookieName = "theme";
+			if (Request.Cookies[cookieName] is null) {
+				Response.Cookies.Append(cookieName, "dark");
+			}
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error() {
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+		[BindProperty]
+		public IFormFile Upload { get; set; }
+		public async Task OnPostAsync() {
+			var file = Path.Combine(_environment.ContentRootPath, "uploads", Upload.FileName);
+			using (var fileStream = new FileStream(file, FileMode.Create)) {
+				await Upload.CopyToAsync(fileStream);
+			}
+		}
+	}
+}
