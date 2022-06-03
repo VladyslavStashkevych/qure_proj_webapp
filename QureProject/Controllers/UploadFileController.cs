d013a054c80690a1aaa7b0d7925f49b291d913ec
Deadmarvald <90569835+Deadmarvald@users.noreply.github.com>
2022-06-03 13:38:59 +0300

    Add files via upload

---
+++ QureProject/Controllers/UploadFileController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QureProject.Controllers {
+	public class UploadFileController : Controller {
+		public IActionResult Index() {
+			return View();
+		}
+	}
+}
---
+++ QureProject/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace QureProject.Models {
+	public class ErrorViewModel {
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
